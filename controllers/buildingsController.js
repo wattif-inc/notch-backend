56765a006baeb41becd2f71e571461af593087bb
Peter chege <peterchege442@gmail.com>
2025-03-28 17:51:55 +0300

    endpoint for fetching all building belonging to a specific organisation

--- controllers/buildingsController.js
+++ controllers/buildingsController.js
@@ -1,6 +1,5 @@
 import faunaClient from "../database/conn.js";
 import { fql, ServiceError } from "fauna";
-import { getInvitationData } from "../helpers/faunadb.js";
 
 export const createBuilding = async (req, res) => {
   const {
@@ -52,6 +51,39 @@ export const createBuilding = async (req, res) => {
     console.error("Error creating building:", error);
     res.status(500).json({
       error: "An error occurred while creating the building",
+    });
+  }
+};
+
+export const getAllBuildings = async (req, res) => {
+  const organizationId = req.params.id;
+
+  if (!organizationId) {
+    return res.status(400).json({ error: "Organization ID is required" });
+  }
+
+  try {
+    const getAllBuildingsQuery = fql`
+      buildings.all().where(building => building.organizationId == ${organizationId})
+    `;
+
+    const result = await faunaClient.query(getAllBuildingsQuery);
+    res.status(200).json({
+      message: "Buildings retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      console.error("Fauna error:", error);
+      return res.status(500).json({
+        msg: "An error occurred while retrieving the buildings",
+        error,
+      });
+    }
+
+    console.error("Error retrieving buildings:", error);
+    res.status(500).json({
+      error: "An error occurred while retrieving the buildings",
     });
   }
 };
--- router/buildings.routes.js
+++ router/buildings.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createBuilding } from "../controllers/buildingsController.js";
+import {
+  createBuilding,
+  getAllBuildings,
+} from "../controllers/buildingsController.js";
 
 const router = Router();
 
 // router.route("/").get(getAllSpacesWithDevices);
 router.route("/create").post(createBuilding);
+router.route("/:id").get(getAllBuildings);
 
 export default router;
